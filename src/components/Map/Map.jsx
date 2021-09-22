456e477fda9a80e33fa792f3ce4453e71cd2bd62
Farzana Akter Shampa <61857690+farzanashampa7@users.noreply.github.com>
2021-09-22 17:27:00 -0400

    Added header component

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import GoogleMapReact from "google-map-react";
+// import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+// import { LocationOnOutlined } from "@material-ui/icons";
+// import { Rating } from "@material-ui/lab";
+
+// import useStyles from "./styles.js";
+
+const Map = () => {
+  // const classes = useStyles();
+  return <h1>Map</h1>;
+};
+
+export default Map;
