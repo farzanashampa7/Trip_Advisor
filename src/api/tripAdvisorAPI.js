f0af2026d5ce386995f0b885db15055916b61c18
Farzana Akter Shampa <61857690+farzanashampa7@users.noreply.github.com>
2021-09-23 16:02:58 -0400

    Fetched data from api

---
+++ src/api/tripAdvisorAPI.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const URL =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+
+export const getPlacesData = async (sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(URL, {
+      params: {
+        bl_latitude: sw.lat,
+        tr_latitude: ne.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+      },
+      headers: {
+        "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+        "x-rapidapi-key": "f6e068ba9fmshb8f9f0273365e87p1d26f7jsnbf56780da694",
+      },
+    });
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
