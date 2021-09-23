1f19aecd56baa3609bb03c0b1a4e30c1429e5cdb
Farzana Akter Shampa <61857690+farzanashampa7@users.noreply.github.com>
2021-09-23 14:21:32 -0400

    Set google map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,14 +1,30 @@
 import React from "react";
-// import GoogleMapReact from "google-map-react";
-// import { Paper, Typography, useMediaQuery } from "@material-ui/core";
-// import { LocationOnOutlined } from "@material-ui/icons";
-// import { Rating } from "@material-ui/lab";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import { LocationOnOutlined } from "@material-ui/icons";
+import { Rating } from "@material-ui/lab";
 
-// import useStyles from "./styles.js";
+import useStyles from "./styles.js";
 
 const Map = () => {
-  // const classes = useStyles();
-  return <h1>Map</h1>;
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width: 600px)");
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyCWTSuZz-Wt_7x7fvrDMYJKbc0tg-b95bo" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
 };
 
 export default Map;
